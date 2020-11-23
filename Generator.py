ab15785378883eb0c304a4666f83b3e655a11a1f
rococoscout <rococoscout@gmail.com>
2020-11-22 21:39:34 -0500

    start format of generator

---
+++ Generator.py
@@ -0,0 +1,21 @@
+# Title: Generate the Song
+# Author: Jess Lonetti/Holly Koerwer
+# Purpose: Generate song based on dataset statistics
+#
+# Contact Information: rococoscout@gmail.com
+# ********************************************************************************
+
+
+def generate_song(phrase,artist):
+    return
+
+
+def generate_structure():
+    return
+
+def generate_line():
+    return
+
+
+if __name__ == "__main__":
+    print("success")
